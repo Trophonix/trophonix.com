9b4db41293d7ecac03c5b7e9b50b89aa0bccbd5c
Lucas Cosolo <502badgamer@gmail.com>
2018-01-20 17:54:28 -0600

    directory listing at /public

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 app.use(favicon(path.join(__dirname, '/public/images/favicon.ico')))
 
 app.use(express.static(path.join(__dirname, '/public')))
+app.use('/public/', express.directory(path.join(__dirname, '/public')))
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Home' })
